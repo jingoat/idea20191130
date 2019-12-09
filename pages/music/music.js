90fd22b823f96a2c1c25ddce7606acae174da0da
zing <806128271@qq.com>
2019-12-09 23:39:26 +0800

    music api method

--- pages/music/music.js
+++ pages/music/music.js
@@ -1,8 +1,42 @@
 // pages/music/music.js
 const InnerAudioContext = wx.createInnerAudioContext();
 Page({
+  getMusicInfo:function(){
+    const searchUrl ='https://c.y.qq.com/soso/fcgi-bin/client_search_cp?aggr=1&cr=1&flag_qc=0';
+    wx.request({
+      url: searchUrl, 
+      data: {
+        p: 1,
+        n: 1,
+        w:'桥边姑娘'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data);
+      }
+    })
+  },
+  getMusicToken: function (songmid) {
+    const searchUrl = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?format=json205361747&platform=yqq&cid=205361747&guid=126548448';
+    wx.request({
+      url: searchUrl,
+      data: {
+        songmid: songmid,
+        filename: 'C400' + songmid+'.m4a' 
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data);
+        const musicUrl = 'http://ws.stream.qqmusic.qq.com/' + filename + '?fromtag=0&guid=126548448&vkey=' + res.data.vkey
+      }
+    })
+  },
   playMusic:function(){
-    InnerAudioContext.src = 'http://music.163.com/song/media/outer/url?id=476592630.mp3';
+    InnerAudioContext.src = 'http://music.163.com/song/media/outer/url?id=1346496466.mp3';
     InnerAudioContext.play();
     InnerAudioContext.onPlay(() => {
       console.log('开始播放')
